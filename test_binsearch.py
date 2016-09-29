028c846e481ae3d95e883fd7e705c5889cace060
Omar A <omar.abboud7@gmail.com>
2016-09-29 17:09:02 -0400

    fixing tests

--- test_binsearch.py
+++ test_binsearch.py
@@ -5,11 +5,14 @@ from binsearch import binary_search
 def test_binsearch():
     assert binary_search([5], 5) == 0
 
+def test_notthere():
+    assert binary_search([1,3,5], 2) == False
+
 def test_char():
     with raises(TypeError):
         binary_search(['a',3])
     
 def test_zero_value():
     with raises(ValueError):
-        binary_search([0,0])
+        binary_search([0,0], 0)
         
