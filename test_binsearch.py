55ac464cf249a9c575c645c8c9f07e91f3c3b6d3
Omar A <omar.abboud7@gmail.com>
2016-09-29 16:45:49 -0400

    travis integration

---
+++ test_binsearch.py
@@ -0,0 +1,15 @@
+
+from pytest import raises
+from binsearch import binary_search
+
+def test_binsearch():
+    assert binary_search([5], 5) == 0
+
+def test_char():
+    with raises(TypeError):
+        binary_search(['a',3])
+    
+def test_zero_value():
+    with raises(ValueError):
+        binary_search([])
+        
